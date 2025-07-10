43cdf97c7d3160ba296536311c54e3714f56f9a6
IsmaelFrancis <frismaellsilva180@gmail.com>
2025-07-10 01:32:32 -0300

    Create DisciplinaApplication.java
    Inicio do projeto Disciplina de desenvolvimento web

---
+++ DisciplinaApplication.java
@@ -0,0 +1,13 @@
+package br.ufra.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciplinaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciplinaApiApplication.class, args);
+	}
+
+}
